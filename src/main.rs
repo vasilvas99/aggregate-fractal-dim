6fe14fe9a2c75ababbcd263a001785c18a1e3102
Vasil Ivanov <vasil.ivanov3@bosch.io>
2023-12-25 18:24:23 +0200

    Make fractal dim calc parallel

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use clap::Parser;
 use fractal_analysis::*;
 use ndarray::{Array4, ArrayView3};
 use npyz::Deserialize;
+use rayon::prelude::*;
 
 static ARR_DEFAULT_NAME: &str = "arr_0";
-static MAX_VAL: i32 = 255;
 
 /// A CLI tool that takes 3D+t aggregation simulations
 /// as 4D *.npz matrices and calculates the fractal dimension
@@ -36,8 +36,8 @@ struct CsvRecord {
 
 fn load_aggregate_data<T: Deserialize>(file_path: impl AsRef<Path>) -> Result<Array4<T>> {
     use ndarray::ShapeBuilder;
-
-    let mut npz = npyz::npz::NpzArchive::open(file_path)?;
+    let file = std::io::Cursor::new(std::fs::read(file_path)?); // Read the whole file in one shot
+    let mut npz = npyz::npz::NpzArchive::new(file)?;
     let arr = npz
         .by_name(ARR_DEFAULT_NAME)?
         .ok_or_else(|| anyhow!("Could not load array by name {}", ARR_DEFAULT_NAME))?;
@@ -54,35 +54,40 @@ fn load_aggregate_data<T: Deserialize>(file_path: impl AsRef<Path>) -> Result<Ar
     Ok(ndarray::Array4::from_shape_vec(true_shape, data)?)
 }
 
-fn threshold(x: &i32) -> i32 {
+fn threshold(x: &i32) -> u8 {
     if *x < 2 {
-        return 0;
+        return u8::MIN;
     }
-    return MAX_VAL;
+    return u8::MAX;
 }
 
 fn calculate_fractal_dimension_3d(frame: ArrayView3<i32>) -> f64 {
     let frame = frame.map(threshold);
     let s = frame.shape();
-    let w = s[0];
-    let l = s[1];
-    let h = s[2];
+    let x_max = s[0];
+    let y_max = s[1];
+    let z_max = s[2];
+    let buf = frame.into_raw_vec().into_par_iter().enumerate();
 
-    let get_key_from_sample = |(coords, val): ((usize, usize, usize), &i32)| -> u32 {
-        let (x, y, z) = coords;
+    let get_key_from_sample = |(flattened_coord, val): (usize, u8)| -> u32 {
+        let mut idx = flattened_coord;
+        let z = idx / (x_max * y_max);
+        idx -= z * x_max * y_max;
+        let y = idx / x_max;
+        let x = idx % x_max;
+
         let normalise_as_u8 = |q, max, min| ((q - min) * 256 / max) as u8;
-        let norm_x = |x| normalise_as_u8(x, w, 0);
-        let norm_y = |y| normalise_as_u8(y, l, 0);
-        let norm_z = |z| normalise_as_u8(z, h, 0);
+        let norm_x = |x| normalise_as_u8(x, x_max, 0);
+        let norm_y = |y| normalise_as_u8(y, y_max, 0);
+        let norm_z = |z| normalise_as_u8(z, z_max, 0);
 
-        // Safety: Ensure values are clipped to 255;
-        let arr = [norm_x(x), norm_y(y), norm_z(z), *val as u8];
-        morton_encoding::morton_encode(arr)
+        let arr = [norm_x(x), norm_y(y), norm_z(z), val];
+        lindel::morton_encode(arr)
     };
 
-    let clzs = get_clzs(frame.indexed_iter(), get_key_from_sample);
-    let (tmp, lacun) = get_results_from_clzs(clzs);
-    let res = finalise_results::<32>(tmp, lacun, w * l * h, 8);
+    let clzs = get_clzs_par(buf, get_key_from_sample).collect::<Vec<_>>();
+    let (tmp, lacun) = get_results_from_clzs(clzs.into_iter());
+    let res = finalise_results::<32>(tmp, lacun, x_max * y_max * z_max, 8);
     res.0
 }
 
@@ -102,7 +107,6 @@ fn main() -> Result<()> {
     let output_file = std::fs::File::create(cli.output_file)?;
     let mut wtr = csv::WriterBuilder::new()
         .delimiter(get_separator(cli.csv_separator)?)
-        .quote_style(csv::QuoteStyle::NonNumeric)
         .from_writer(output_file);
 
     for (frame_number, frame) in l.outer_iter().enumerate() {
